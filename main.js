8fc04ec45aa1ba95c5f3fa2197d7a7c76d37bf4f
jdegiovanni <jdegiovanni@workfusion.com>
2022-12-21 12:27:46 -0600

    export data functionality

--- main.js
+++ main.js
@@ -135,3 +135,20 @@ function declareWinner(winner) {
 loadPlayers();
 let game = null;
 createGame();
+
+function exportData() {
+    download('players.json', JSON.stringify(players));
+}
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
